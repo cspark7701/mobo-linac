7d0fca8531c00a24e53452c62a7a4dda00703f1d
Chong Shik Park <cspark7701@gmail.com>
2026-07-30 10:58:07 +0900

    add environment setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export ASTRA_BIN=/home/cspark/Work/projects/mobo_linac/bin/astra
+export GENERATOR_BIN=/home/cspark/Work/projects/mobo_linac/bin/generator
+
+export PATH=/home/cspark/Work/projects/mobo_linac/bin/:$PATH
