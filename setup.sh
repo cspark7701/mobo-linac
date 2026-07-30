864e775f545236248cff5ec67e7d6f81cfbb8904
Chong Shik Park <cspark7701@gmail.com>
2026-07-30 11:00:11 +0900

    change conda env

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ export GENERATOR_BIN=/home/cspark/Work/projects/mobo_linac/bin/generator
 
 export PATH=/home/cspark/Work/projects/mobo_linac/bin/:$PATH
 
-conda activate pyat-dev
+conda activate linac-opt
