6deb0f892060ce70aac2b22a5e6902ec2ed862ff
Chong Shik Park <cspark7701@gmail.com>
2026-07-30 10:58:36 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ export ASTRA_BIN=/home/cspark/Work/projects/mobo_linac/bin/astra
 export GENERATOR_BIN=/home/cspark/Work/projects/mobo_linac/bin/generator
 
 export PATH=/home/cspark/Work/projects/mobo_linac/bin/:$PATH
+
+conda activate pyat-dev
